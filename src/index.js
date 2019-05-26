889090e18ebff4710cce367a8b1c30008a95994e
= <jo123.garcia.s@gmail.com>
2019-05-25 19:31:36 -0500

    structure refactor to npm package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState } from 'react';
+import { render } from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { useFormValidator } from './lib';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  const { changeValidator } = useFormValidator();
+  const [state, setState] = useState({
+    form_valid: false,
+    number: {
+      valid: true,
+      value: '',
+      type: 'number',
+      rules: {
+        format: 'int',
+      },
+    },
+    text: {
+      valid: true,
+      value: '',
+      type: 'text',
+      rules: {
+        max_size: 20,
+        min_size: 5,
+      },
+    },
+    pattern: {
+      valid: true,
+      value: '',
+      type: 'pattern',
+      pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+      rules: {
+        max_size: 10,
+        min_size: 5,
+        clear_max_size_error: true,
+      },
+    },
+  });
+
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const status = changeValidator(target, state);
+    setState(status);
+  };
+
+  const { number, text, pattern } = state;
+
+  return (
+    <div>
+      <form>
+        <div>
+          <label>Numero</label>
+          <input name="number" onChange={handleChange} value={number.value} />
+          <div>{ number.error_message }</div>
+        </div>
+        <div>
+          <label>Texto</label>
+          <input type="text" name="text" onChange={handleChange} value={text.value} />
+          <div>{ text.error_message }</div>
+        </div>
+        <div>
+          <label>Pattern</label>
+          <input type="text" name="pattern" onChange={handleChange} value={pattern.value} />
+          <div>{ pattern.error_message }</div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('root'));
---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+import useFormValidator from './hooks/useFormValidator';
+
+export {
+  // eslint-disable-next-line import/prefer-default-export
+  useFormValidator,
+};
